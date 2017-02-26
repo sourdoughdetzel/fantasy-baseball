906f5c2f7b29bedfc99c1651242cfdf70f607fef
Ben Zobrist <detzelteets@gmail.com>
2017-02-25 20:00:24 -0500

    All kinds of shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MaterialModule} from "@angular/material";
+import { RouterModule, Routes } from '@angular/router';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TabsComponent} from "./tabs/tabs.component";
+import { TeamSetupComponent } from "./team-setup/team-setup.component";
+import { RFAProcessComponent } from "./rfa-process/rfa-process.component";
+
+const appRoutes: Routes = [
+  { path: 'rfa', component: RFAProcessComponent },
+  { path: 'teams',      component: TeamSetupComponent },
+  { path: '',
+    redirectTo: '/rfa',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TabsComponent,
+    RFAProcessComponent,
+    TeamSetupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
